f615cc28a04dba74b2bf80e58f7ed8d6be669547
Divya R <kwrdivyaraj100@gmail.com>
2023-04-12 17:35:24 +0530

    Function to show elements from localStorage

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 console.log("Welcome to notes app. This is app.js");
-
+showNotes();
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
@@ -14,4 +14,34 @@ addBtn.addEventListener("click", function(e) {
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
   console.log(notesObj);
+  showNotes();
+  
 });
+
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } 
+  else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+    <div class="noteCardmy-2 mx-2 card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Note ${index + 1}</h5>
+              <p class="card-text">${element}</p>
+              <button href="#" class="btn btn-primary">Delete Note</button>
+            </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  }
+  else {
+    notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+  }
+}
